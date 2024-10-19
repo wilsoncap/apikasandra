f48e38769b730871611c122a3f2d8089a0577137
Wilson Capacho <wiston.98@hotmail.com>
2024-10-19 17:44:01 -0500

    auditi en model products

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -1,12 +1,14 @@
 <?php
 
 namespace App\Models;
+use OwenIt\Auditing\Contracts\Auditable;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Product extends Model
+class Product extends Model implements Auditable
 {
+    use \OwenIt\Auditing\Auditable;
     use HasFactory;
 
     protected $fillable = ['name', 'price'];
